35ac5b864a68e86f463bad5721922d1e9f4abcb5
ubekishov <ulan.bekishov@coveros.com>
2016-02-17 11:51:54 -0500

    Revised Chef Server Config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,5 +7,5 @@ node_name                "ubekishov"
 client_key               "#{current_dir}/ubekishov.pem"
 validation_client_name   "coveros-validator"
 validation_key           "#{current_dir}/coveros-validator.pem"
-chef_server_url          "https://ec2-54-210-10-183.compute-1.amazonaws.com/organizations/coveros"
+chef_server_url          "https://ec2-52-90-71-45.compute-1.amazonaws.com/organizations/coveros"
 cookbook_path            ["#{current_dir}/../cookbooks"]
