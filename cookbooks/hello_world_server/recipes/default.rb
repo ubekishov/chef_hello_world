dd237d92763da527f7d355a289360a216ed8f8a4
ubekishov <ulan.bekishov@coveros.com>
2016-02-19 11:56:56 -0500

    Updated default.rb

--- cookbooks/hello_world_server/recipes/default.rb
+++ cookbooks/hello_world_server/recipes/default.rb
@@ -20,9 +20,7 @@ service 'tomcat' do
 
 end
 
-# finding docker image that images init find for platform in 
-
-kitchen yml
+# finding docker image that images init find for platform in kitchen yml
 
 # sol 2: 
 
